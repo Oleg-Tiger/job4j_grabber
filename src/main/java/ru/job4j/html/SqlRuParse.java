c746104d3c2774f92d8bd8902038f47c6344f263
Oleg-Tiger <free_person@inbox.ru>
2021-08-05 17:39:01 +0300

    2.4. SqlRuParse [#285213 #185553]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -11,14 +11,12 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class SqlRuParse implements Parse {
 
     private final DateTimeParser dateTimeParser;
-    private Map<String, String> tittles = new HashMap<>();
 
     public SqlRuParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
@@ -26,9 +24,11 @@ public class SqlRuParse implements Parse {
 
     public static void main(String[] args) throws Exception {
         SqlRuParse srp = new SqlRuParse(new SqlRuDateTimeParser());
-        List<Post> list = srp.list("https://www.sql.ru/forum/job-offers");
-        Post post =  srp.detail(list.get(9).getLink());
+        Post post = srp.detail("https://www.sql.ru/forum/1336747/programmist-ms-sql-udalyonno-150-200tr-net");
         System.out.println(post);
+        System.out.println();
+        Post post2 = srp.detail("https://www.sql.ru/forum/1332112-1/programmist-prikladnogo-po-c-po-produktu-polator");
+        System.out.println(post2);
     }
 
     @Override
@@ -45,7 +45,6 @@ public class SqlRuParse implements Parse {
                         "%s%sДата обновления: %s", href.text(), System.lineSeparator(), rowDate.get(i).text()
                         );
                 rsl.add(new Post(title, postLink));
-                this.tittles.put(postLink, title);
             }
         }
         return rsl;
@@ -54,9 +53,27 @@ public class SqlRuParse implements Parse {
         @Override
         public Post detail(String link) throws IOException {
         Document doc = Jsoup.connect(link).get();
-        String description =  doc.select("table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(2)").text();
-        String created = doc.select("table:nth-child(3) > tbody > tr:nth-child(3)").text().split(" \\[")[0];
-        return new Post(this.tittles.get(link), link, description, this.dateTimeParser.parse(created));
+        String table = "table:nth-child(3)";
+        Elements pages = doc.select("#content-wrapper-forum > table:nth-child(2) > tbody > tr > td > a");
+        if (pages.size() != 0) {
+            table = "table:nth-child(4)";
+        }
+        String description =  doc.select(table.concat(" > tbody > tr:nth-child(2) > td:nth-child(2)")).text();
+        String created = doc.select(table.concat(" > tbody > tr:nth-child(3)")).text().split(" \\[")[0];
+        String name = doc.select(table.concat(" > tbody > tr:nth-child(1)")).get(0).text().replace(" [new]", "");
+        if (pages.size() != 0) {
+            doc = createDocForLastPage(pages);
+        }
+        Elements rowDate = doc.select("tr:nth-child(3) > td");
+        String title = String.format(
+                    "%s%sДата обновления: %s", name, System.lineSeparator(), rowDate.get(rowDate.size() - 1).text().split(" \\[")[0]
+            );
+            return new Post(title, link, description, this.dateTimeParser.parse(created));
+    }
+
+    private Document createDocForLastPage(Elements pages) throws IOException {
+            String linkOfLastPage = pages.get(pages.size() - 3).attr("href");
+            return Jsoup.connect(linkOfLastPage).get();
     }
 }
 
