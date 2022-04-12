ad5242dc97df1b2a7463539eedb3bb30880cbf43
Oleg-Tiger <free_person@inbox.ru>
2022-04-12 23:34:11 +0300

    2. Парковка машин [#853] - Интерфейсы.

---
+++ src/main/java/ru/job4j/design/lsp/parking/Parking.java
@@ -0,0 +1,6 @@
+package ru.job4j.design.lsp.parking;
+
+public interface Parking {
+    void addCar(Car car);
+    void removeCar(Car car);
+}
---
+++ src/main/java/ru/job4j/design/lsp/parking/ParkingService.java
@@ -0,0 +1,8 @@
+package ru.job4j.design.lsp.parking;
+
+public class ParkingService {
+    private Parking parking;
+    boolean parkCar(Car car) {
+        return false;
+    }
+}
